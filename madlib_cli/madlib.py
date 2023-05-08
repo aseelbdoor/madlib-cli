cff93f55ac54c67de2d01aac95f3545986ce52a2
aseelbdoor <aseel.bdoor2001@gmail.com>
2023-05-08 17:58:44 +0000

    set files

---
+++ madlib_cli/madlib.py
@@ -0,0 +1,10 @@
+def read_template(bath):
+    with open(bath,'r') as f:
+        return f.read()
+def parse_template():
+    pass
+def merge():
+    pass
+
+#main 
+print(read_template('./assets/dark_and_stormy_night_template.txt'))
